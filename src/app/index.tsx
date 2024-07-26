ab0ad506a29d0cbbfda5ee0b73088717036c6b08
Zetharion <zetharionshim@gmail.com>
2024-07-26 23:40:00 +0300

    chore: styles scss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Routing } from '@pages/index'
+import './styles/index.scss'
 
 export const App = () => {
 	return (
