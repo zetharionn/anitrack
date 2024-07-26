a2829bac1d20f1632100743f9324e2d554a7d74f
Zetharion <zetharionshim@gmail.com>
2024-07-26 06:02:49 +0300

    feat: routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,14 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Routing } from '@pages/index'
+
 export const App = () => {
-	return <div>App</div>
+	return (
+		<BrowserRouter>
+			<SpeedInsights />
+			<Routing />
+			<Analytics />
+		</BrowserRouter>
+	)
 }
