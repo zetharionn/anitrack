c303ccb7d8de91963d60f7fbd160fdd9b2118649
Zetharion <zetharionshim@gmail.com>
2024-07-26 05:09:24 +0300

    feat!: init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared')
+		}
+	}
+})
