eec5b3928bb6d3d8870064e34e9692a3c9fa8f81
Zetharion <zetharionshim@gmail.com>
2024-07-26 18:03:24 +0300

    refactor(routing): browserRouter in routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,13 @@
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { BrowserRouter } from 'react-router-dom'
 import { Routing } from '@pages/index'
 
 export const App = () => {
 	return (
-		<BrowserRouter>
+		<>
 			<SpeedInsights />
 			<Routing />
 			<Analytics />
-		</BrowserRouter>
+		</>
 	)
 }
