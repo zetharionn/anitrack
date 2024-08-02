d43121ac9a9134c0f2e66883519aabcb2c2d2d0a
Zetharion <zetharionshim@gmail.com>
2024-08-02 09:21:56 +0300

    feat: home background

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -5,6 +5,7 @@ import { ROUTES } from '@shared/lib'
 const Home: FC = () => {
 	return (
 		<div className='flex flex-col text-center items-center justify-center gap-4 h-[85vh]'>
+			<div className='absolute z-[-1] w-[256px] h-[256px] rounded-full bg-gradient-to-b from-[#f5d63b] to-[#9739f0] blur-[96px]'></div>
 			<h1 className='sm:text-3xl md:text-5xl lg:text-6xl font-extrabold'>
 				<span>Track your Anime Stats on </span>
 				<span className='text-transparent bg-clip-text bg-gradient-to-br from-[#f5d63b] to-[#9739f0]'>
