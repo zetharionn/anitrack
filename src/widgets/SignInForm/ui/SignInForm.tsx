2708cabe225299368ddd05a1c1eef0c6fdb95088
Zetharion <zetharionshim@gmail.com>
2024-08-13 19:50:08 +0300

    refactor: signUpForm & signInForm

--- src/widgets/SignInForm/ui/SignInForm.tsx
+++ src/widgets/SignInForm/ui/SignInForm.tsx
@@ -1,25 +1,38 @@
+import { AuthFormTypes, useAuthForm } from '@features/AuthForm'
 import { Button, Input } from '@nextui-org/react'
 
 export const SignInForm = () => {
+	const {
+		onSubmit,
+		register,
+		formState: { errors }
+	} = useAuthForm(AuthFormTypes.SignInForm)
+
 	return (
 		<div className='mx-auto flex flex-col max-w-xs'>
-			<div className='flex flex-col gap-4'>
+			<form className='flex flex-col gap-4' onSubmit={onSubmit}>
 				<Input
+					{...register('email')}
 					isRequired
 					type='email'
 					radius='md'
 					variant='bordered'
 					label='Email'
+					isInvalid={!!errors.email}
+					errorMessage={errors.email?.message}
 				/>
 				<Input
+					{...register('password')}
 					isRequired
 					type='password'
 					radius='md'
 					variant='bordered'
 					label='Password'
+					isInvalid={!!errors.password}
+					errorMessage={errors.password?.message}
 				/>
 				<Button radius='sm'>Sign In</Button>
-			</div>
+			</form>
 		</div>
 	)
 }
