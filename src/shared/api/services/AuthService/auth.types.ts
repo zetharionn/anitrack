2b1f29e9f4c31cd898dfca76a52c35dc7d580f6e
Zetharion <zetharionshim@gmail.com>
2024-09-01 00:03:39 +0300

    refactor: auth service types

--- src/shared/api/services/AuthService/auth.types.ts
+++ src/shared/api/services/AuthService/auth.types.ts
@@ -1,9 +1,9 @@
-import { Session, SupabaseClient } from '@supabase/supabase-js'
+import type { SupabaseClient, Session } from '@supabase/supabase-js'
 
 export interface IAuthService {
 	readonly supabaseClient: SupabaseClient
 	signUp: (email: string, password: string) => void
 	signIn: (email: string, password: string) => void
 	signOut: () => void
-	getSession: () => Promise<{ session: Session } | { session: null }>
+	getSession: () => Promise<Session | null>
 }
