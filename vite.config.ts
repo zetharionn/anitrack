412a39c5f95c1b2423a0ef653562af9c45850e5d
zetharionn <zetharionshim@gmail.com>
2024-09-22 07:58:14 +0300

    fix: sass new api using

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern',
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			'@app': path.resolve(__dirname, './src/app'),
