993c73812385e02258504a9b0197ea3a369d3f85
zetharionn <zetharionshim@gmail.com>
2024-09-23 06:37:53 +0300

    chore: tailwind config

---
+++ apps/frontend/tailwind.config.ts
@@ -0,0 +1,12 @@
+import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss'
+
+export default {
+	content: [
+		'./index.html',
+		'./src/**/*.{ts,tsx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+	],
+	darkMode: 'class',
+	plugins: [nextui({ defaultTheme: 'dark' })],
+} satisfies Config
