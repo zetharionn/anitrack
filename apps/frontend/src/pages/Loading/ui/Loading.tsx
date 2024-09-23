7bed9e9ea3cbbb9fba7db82e528778a833bc011c
zetharionn <zetharionshim@gmail.com>
2024-09-23 20:09:29 +0300

    feat: loading page

---
+++ apps/frontend/src/pages/Loading/ui/Loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from '@nextui-org/react'
+
+const Loading = () => {
+	return (
+		<div className='flex items-center justify-center h-[85vh]'>
+			<Spinner size='lg' />
+		</div>
+	)
+}
+export default Loading
