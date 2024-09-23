c67a98634794268a1742dcb5215d3e92c31cef03
zetharionn <zetharionshim@gmail.com>
2024-09-23 20:09:44 +0300

    feat: home page

---
+++ apps/frontend/src/pages/Home/ui/Home.tsx
@@ -0,0 +1,40 @@
+import { Button, Link } from '@nextui-org/react'
+import { ROUTES } from '@shared/lib'
+
+const Home = () => {
+	return (
+		<div className='flex flex-col text-center items-center justify-center gap-4 h-[85vh]'>
+			<div className='absolute z-[-1] w-[256px] h-[256px] rounded-full bg-gradient-to-b from-[#f5d63b] to-[#9739f0] blur-[96px]'></div>
+			<h1 className='sm:text-3xl md:text-5xl lg:text-6xl font-extrabold'>
+				<span>Watch and Track on </span>
+				<span className='text-transparent bg-clip-text bg-gradient-to-br from-[#f5d63b] to-[#9739f0]'>
+					AniTrack
+				</span>
+			</h1>
+			<p className='md:text-xl lg:text-2xl'>
+				Watch your favorite anime and track watch time!
+			</p>
+			<div className='flex flex-row gap-4'>
+				<Button
+					className='font-bold text-white'
+					color='primary'
+					variant='shadow'
+					size='lg'
+					as={Link}
+					href={ROUTES.SIGN_UP}>
+					Start Tracking
+				</Button>
+				<Button
+					variant='bordered'
+					size='lg'
+					as={Link}
+					href='https://github.com/zetharionn/AniTrack'
+					target='_blank'>
+					Github
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export default Home
