8693bc46fc5c25b99a4169be0da2e383469f00cd
zetharionn <zetharionshim@gmail.com>
2024-09-23 06:41:21 +0300

    chore: app

--- apps/frontend/src/app/index.tsx
+++ apps/frontend/src/app/index.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Routing } from './ui/routing'
+import { Analytics } from '@vercel/analytics/react'
+import './ui/styles/index.scss'
+
 export const App = () => {
-	return <div>App</div>
+	return (
+		<BrowserRouter>
+			<SpeedInsights />
+			<Routing />
+			<Analytics />
+		</BrowserRouter>
+	)
 }
 
 export default App
