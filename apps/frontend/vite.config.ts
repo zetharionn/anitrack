27520e43ae178260c7f5eb804144cd958d638c22
zetharionn <zetharionshim@gmail.com>
2024-09-28 20:58:37 +0300

    fix: error on fail

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,29 +1,33 @@
-import react from '@vitejs/plugin-react-swc'
-import biomePlugin from 'vite-plugin-biome';
-import { defineConfig } from 'vite'
-import path from 'path'
+import react from "@vitejs/plugin-react-swc";
+import biomePlugin from "vite-plugin-biome";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-	plugins: [react(), biomePlugin({
-		mode: 'check',
-		files: './src/*.*',
-		applyFixes: true,
-	})],
+	plugins: [
+		react(),
+		biomePlugin({
+			mode: "check",
+			files: "./src/*.*",
+			applyFixes: true,
+			failOnError: false,
+		}),
+	],
 	css: {
 		preprocessorOptions: {
 			scss: {
-				api: 'modern',
+				api: "modern",
 			},
 		},
 	},
 	resolve: {
 		alias: {
-			'@app': path.resolve(__dirname, './src/app'),
-			'@pages': path.resolve(__dirname, './src/pages'),
-			'@widgets': path.resolve(__dirname, './src/widgets'),
-			'@features': path.resolve(__dirname, './src/features'),
-			'@entities': path.resolve(__dirname, './src/entities'),
-			'@shared': path.resolve(__dirname, './src/shared'),
+			"@app": path.resolve(__dirname, "./src/app"),
+			"@pages": path.resolve(__dirname, "./src/pages"),
+			"@widgets": path.resolve(__dirname, "./src/widgets"),
+			"@features": path.resolve(__dirname, "./src/features"),
+			"@entities": path.resolve(__dirname, "./src/entities"),
+			"@shared": path.resolve(__dirname, "./src/shared"),
 		},
 	},
-})
+});
