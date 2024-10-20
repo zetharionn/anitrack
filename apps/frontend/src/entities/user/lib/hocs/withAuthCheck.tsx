a0f7bd487a10d45f54026702225eb70babdcb774
zetharionn <zetharionn@gmail.com>
2024-10-21 02:39:37 +0300

    refactor: use checkAuth

--- apps/frontend/src/entities/user/lib/hocs/withAuthCheck.tsx
+++ apps/frontend/src/entities/user/lib/hocs/withAuthCheck.tsx
@@ -1,20 +1,15 @@
-import { useAuthStore } from '@entities/user/model'
-import { authService } from '@shared/api'
 import { type ComponentType, useEffect } from 'react'
+import { useAuthStore } from '@entities/user'
 
 export const withAuthCheck = <T extends object>(
 	Component: ComponentType<T>
 ) => {
 	const WrappedComponent = (props: T) => {
-		const signOut = useAuthStore(state => state.signOut)
+		const checkAuth = useAuthStore(state => state.checkAuth)
 
 		useEffect(() => {
-			authService.getUser().then(res => {
-				if (res.error) {
-					signOut()
-				}
-			})
-		}, [signOut])
+			checkAuth()
+		}, [checkAuth])
 
 		return <Component {...props} />
 	}
