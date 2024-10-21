f82ada53f3f0cebc767d18d80d5747d42b78d140
zetharionn <zetharionn@gmail.com>
2024-10-21 15:45:50 +0300

    refactor: simplify

--- apps/frontend/src/features/ProtectedRoute/lib/ProtectedRoute.tsx
+++ apps/frontend/src/features/ProtectedRoute/lib/ProtectedRoute.tsx
@@ -1,19 +1,15 @@
 import { useAuthStore } from '@entities/user'
 import { ROUTES } from '@shared/lib'
 import { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
 
 export const ProtectedRoute = () => {
 	const checkAuth = useAuthStore(state => state.checkAuth)
 	const isAuth = useAuthStore(state => state.isAuth)
-	const navigate = useNavigate()
 
 	useEffect(() => {
 		checkAuth()
-		if (isAuth === false) {
-			navigate(ROUTES.HOME)
-		}
-	}, [checkAuth, isAuth, navigate])
+	}, [checkAuth])
 
-	return <Outlet />
+	return isAuth ? <Outlet /> : <Navigate to={ROUTES.HOME} />
 }
